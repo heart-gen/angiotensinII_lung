67e077f0dc930d394fa27145060c7fddaf50b4a2
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-03-29 08:47:27 -0400

    adding scripts for downloading HLCA, version 2

---
+++ inputs/hlca/_h/01.download_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Downloading data"
+wget https://github.com/LungCellAtlas/HLCA/blob/main/docs/HLCA_metadata_explanation.csv
+curl -o hlca_full.h5ad https://datasets.cellxgene.cziscience.com/3ab47484-a3eb-4f6a-beea-670e1a8fc1e8.h5ad
+curl -o hlca_core.h5ad https://datasets.cellxgene.cziscience.com/7a3f08f9-5d07-4ddd-a8fe-5967dd34f35f.h5ad
+
+echo "Downloading models"
+wget https://zenodo.org/records/7599104/files/HLCA_full_v1.1_emb.h5ad
+wget https://zenodo.org/records/7599104/files/HLCA_reference_model.zip
+wget https://zenodo.org/records/7599104/files/HLCA_reference_model_gene_order_ids_and_symbols.csv
+wget https://zenodo.org/records/7599104/files/HLCA_surgery_models.zip
---
+++ inputs/hlca/_h/step_1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --partition=shared,bluejay
+#SBATCH --job-name=download_data
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jbenja13@jh.edu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10gb
+#SBATCH --output=download.log
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## List current modules for reproducibility
+
+module list
+
+## Edit with your job command
+
+bash ../_h/01.download_data.sh
+
+echo "**** Job ends ****"
+date
