7d870075575df1016ccceb4fe557e85393fcb70d
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-04-12 17:36:10 -0400

    adding seurat object to download

--- inputs/hlca/_h/01.download_data.sh
+++ inputs/hlca/_h/01.download_data.sh
@@ -4,6 +4,7 @@ echo "Downloading data"
 wget https://github.com/LungCellAtlas/HLCA/blob/main/docs/HLCA_metadata_explanation.csv
 curl -o hlca_full.h5ad https://datasets.cellxgene.cziscience.com/3ab47484-a3eb-4f6a-beea-670e1a8fc1e8.h5ad
 curl -o hlca_core.h5ad https://datasets.cellxgene.cziscience.com/7a3f08f9-5d07-4ddd-a8fe-5967dd34f35f.h5ad
+curl -o hlca_core.rds https://datasets.cellxgene.cziscience.com/2aa90e63-9a6d-444d-8343-8fc2a9921797.rds
 
 echo "Downloading models"
 wget https://zenodo.org/records/7599104/files/HLCA_full_v1.1_emb.h5ad
