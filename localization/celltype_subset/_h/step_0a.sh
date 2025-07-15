b65f60a3ab9c988d27cbab591a468ec225740f01
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-07-15 11:55:50 -0400

    initial updating for bridges-2

--- localization/celltype_subset/_h/step_0a.sh
+++ localization/celltype_subset/_h/step_0a.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-#SBATCH --partition=shared,bluejay
-#SBATCH --job-name=subsetting
+#SBATCH --partition=RM-shared
+#SBATCH --job-name=core_subset
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=jbenja13@jh.edu
-#SBATCH --nodes=1
+#SBATCH --mail-user=kj.benjamin90@gmail.com
+#SBATCH --nodes=40
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=200gb
-#SBATCH --output=cluster_subsetting.log
+#SBATCH --output=core_subsetting.log
 
 echo "**** Job starts ****"
 date
 
-echo "**** JHPCE info ****"
+echo "**** Bridges-2 info ****"
 echo "User: ${USER}"
 echo "Job id: ${SLURM_JOBID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURM_NODENAME}"
 echo "Hostname: ${HOSTNAME}"
-echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 
 ## List current modules for reproducibility
 
+module purge
 module load R
 module list
 
 echo "**** Run subsetting ****"
-Rscript ../_h/00.subset_ct.R
+export BASILISK_EXTERNAL_DIR=/ocean/projects/bio250020p/shared/opt/basilisk_cache
+
+model="core"
+Rscript ../_h/00.subset_ct.R "$model"
 
 echo "**** Job ends ****"
 date
