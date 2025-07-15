2cf6ff57c771685843307f01a5012f8a5a6061c3
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-07-15 13:32:38 -0400

    revised to reduce compute time based on actual run

--- localization/celltype_subset/_h/step_0.sh
+++ localization/celltype_subset/_h/step_0.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=kj.benjamin90@gmail.com
 #SBATCH --ntasks-per-node=40
-#SBATCH --time=01:30:00
+#SBATCH --time=00:40:00
 #SBATCH --output=core_subsetting.log
 
 log_message() {
