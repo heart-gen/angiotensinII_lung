e93a0e4bede3e60c9742357cbd32ab0ea3b3f91e
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-07-16 09:55:30 -0400

    initial troubleshooting with visualization

--- localization/celltype_subset/_h/02.sub-clustering.py
+++ localization/celltype_subset/_h/02.sub-clustering.py
@@ -31,8 +31,10 @@ def analyze_marker_genes(adata, groupby='leiden', method='wilcoxon'):
     return adata
 
 
-def normalize_marker_expression(adata, markers, ref_adata=None, ref_label='Fibroblasts'):
-    pericyte_expr = adata[:, markers].X.mean(axis=1)
+def normalize_marker_expression(adata, markers, ref_adata=None,
+                                ref_label='Alveolar fibroblasts'):
+    ensembl_markers = adata.var[adata.var.feature_name.isin(markers)].index.values
+    pericyte_expr = adata[:, ensembl_markers].X.mean(axis=1)
     
     if ref_adata is not None and 'cell_type' in ref_adata.obs:
         fibroblast_cells = ref_adata.obs['cell_type'] == ref_label
@@ -51,32 +53,95 @@ def normalize_marker_expression(adata, markers, ref_adata=None, ref_label='Fibro
     return adata
 
 
-def plot_clusters_and_markers(adata, marker_genes, cluster_key='leiden'):
-    sc.pl.umap(adata, color=cluster_key, title='Leiden Clusters')
-    sc.pl.embedding(adata, basis='X_phate', color=cluster_key, title='PHATE Clusters')
+def normalize_by_fibroblast_count(adata, ref_adata, ref_label='Alveolar fibroblasts'):
+    if ref_adata is None or 'cell_type' not in ref_adata.obs:
+        raise ValueError("Reference AnnData (`ref_adata`) with 'cell_type' annotation required.")
+        
+    fibroblast_count = (ref_adata.obs['cell_type'] == ref_label).sum()
+    if fibroblast_count == 0:
+        print(f"Warning: No fibroblast cells labeled '{ref_label}' found. No normalization performed.")
+        return adata  # or adata unchanged
 
+    normalized_total = adata.X.sum(axis=1) / fibroblast_count
+    adata.obs['normalized_total_expression_by_fibroblast'] = normalized_total
+    return adata
+
+
+def plot_clusters_and_markers(adata, marker_genes, cluster_key='leiden'):
+    # Plot Leiden Clusters in a separate figure
+    plt.figure(figsize=(7, 6))
+    sc.pl.umap(adata, color=cluster_key, title='Leiden Clusters', show=True)
+    plt.savefig('leiden_clusters.png')
+    plt.savefig('leiden_clusters.pdf')
+    plt.close()
+    
+    plt.figure(figsize=(7, 6))
+    sc.pl.embedding(adata, basis='X_phate', color=cluster_key, title='PHATE Clusters', show=True)
+    plt.savefig('phate_clusters.png')
+    plt.savefig('phate_clusters.pdf')
+    plt.close()
+
+    # Plot gene expression
     for gene in marker_genes:
-        if gene in adata.var_names:
-            sc.pl.umap(adata, color=gene, title=f'UMAP: {gene} expression')
-            sc.pl.embedding(adata, basis='X_phate', color=gene, title=f'PHATE: {gene} expression')
+        ensembl_id = adata.var[adata.var.feature_name == gene].index.values[0]
+        if ensembl_id in adata.var_names:
+            plt.figure(figsize=(7, 6))
+            sc.pl.umap(adata, color=ensembl_id, title=f'UMAP: {gene} expression', show=True)
+            plt.savefig(gene.lower() + '_umap.png')
+            plt.savefig(gene.lower() + '_umap.pdf')
+            plt.close()
+
+            plt.figure(figsize=(7, 6))
+            sc.pl.embedding(adata, basis='X_phate', color=ensembl_id, title=f'PHATE: {gene} expression', show=True)
+            plt.savefig(gene.lower() + '_phate.png')
+            plt.savefig(gene.lower() + '_phate.pdf')
+            plt.close()
         else:
             print(f"Warning: Gene {gene} not found in dataset")
 
     if 'normalized_pericyte_marker_expr' in adata.obs:
+        plt.figure(figsize=(7, 6))
         sc.pl.umap(adata, color='normalized_pericyte_marker_expr',
-                   title='Normalized Pericyte Marker Expression')
+                   title='Normalized Pericyte Marker Expression', show=True)
+        plt.savefig('pericyte_marker_expression.png')
+        plt.savefig('pericyte_marker_expression.pdf')
+        plt.close()
+        
+        plt.figure(figsize=(7, 6))
+        sc.pl.embedding(adata, basis='X_phate', color='normalized_pericyte_marker_expr',
+                        title='Normalized Pericyte Marker Expression', show=True)
+        plt.savefig('pericyte_marker_expression_phate.png')
+        plt.savefig('pericyte_marker_expression_phate.pdf')
+        plt.close()
+
+    if 'normalized_total_expression_by_fibroblast' in adata.obs:
+        plt.figure(figsize=(7, 6))
+        sc.pl.umap(adata, color='normalized_total_expression_by_fibroblast',
+                   title='Normalized Pericyte Expression', show=True)
+        plt.savefig('pericyte_fibroblast_expression.png')
+        plt.savefig('pericyte_fibroblast_expression.pdf')
+        plt.close()
+
+        plt.figure(figsize=(7, 6))
+        sc.pl.embedding(adata, basis='X_phate',
+                        color='normalized_total_expression_by_fibroblast',
+                        title='Normalized Pericyte Expression', show=True)
+        plt.savefig('pericyte_fibroblast_expression_phate.png')
+        plt.savefig('pericyte_fibroblast_expression_phate.pdf')
+        plt.close()
 
 
 def subcluster_pericytes(
-        adata, ref_adata=None, fibroblast_label='Fibroblasts',
+        adata, ref_adata=None, fibroblast_label='Alveolar fibroblasts',
         pericyte_markers=['HIGD1B', 'PDGFRB', 'CSPG4'],
         marker_genes=['AGTR1', 'ACTA2'], phate_knn=5, phate_decay=40,
-        leiden_resolution=1.0):
-    preprocess_adata(adata)
-    add_phate_embedding(adata, knn=phate_knn, decay=phate_decay)
-    perform_clustering(adata, resolution=leiden_resolution)
-    analyze_marker_genes(adata)
-    normalize_marker_expression(adata, pericyte_markers, ref_adata, fibroblast_label)
+        leiden_resolution=0.5):
+    adata = preprocess_adata(adata)
+    adata = add_phate_embedding(adata, knn=phate_knn, decay=phate_decay)
+    adata = perform_clustering(adata, resolution=leiden_resolution)
+    adata = analyze_marker_genes(adata)
+    adata = normalize_marker_expression(adata, pericyte_markers, ref_adata, fibroblast_label)
+    adata = normalize_by_fibroblast_count(adata, ref_adata, fibroblast_label)
     plot_clusters_and_markers(adata, marker_genes)
     return adata
 
@@ -84,8 +149,9 @@ def subcluster_pericytes(
 def main():
     # Load data
     adata = sc.read_h5ad('../_m/pericyte.hlca_core.dataset.h5ad')
+    ref_adata = sc.read_h5ad("../_m/stroma.hlca_core.dataset.h5ad")
     # Subcluster
-    adata = subcluster_pericytes(adata)
+    adata = subcluster_pericytes(adata, ref_adata, leiden_resolution=0.25)
     # Save the subclusters
     adata.write('pericyte.hlca_core.subclustered.h5ad')
     # Session information
