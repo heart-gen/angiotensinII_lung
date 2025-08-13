2cce924b80d8ddc8312b017a6112afd7a8b58f35
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-08-13 10:28:22 -0500

    minor update

--- localization/celltype_subset/_h/04.colocalization_plot.R
+++ localization/celltype_subset/_h/04.colocalization_plot.R
@@ -1,49 +1,16 @@
 ## This script examines differences in expression
-import session_info
-import scanpy as sc
-import pandas as pd
-import seaborn as sns
-import scikit_posthocs as sp
-from os import makedirs, path
-from scipy.stats import kruskal
-import matplotlib.pyplot as plt
+library(Seurat)
 
-def extract_expression(adata, marker_genes, cluster_key="leiden"):
-    # Get corresponding ensembl ids
-    ensembl_ids = adata.var[adata.var.feature_name.isin(marker_genes)].index.tolist()
-    feature_map = adata.var.loc[ensembl_ids, "feature_name"].to_dict()
-
-    # Subset expression from logcounts layer
-    expr = pd.DataFrame(adata[:, ensembl_ids].layers["logcounts"].toarray(),
-                        columns=ensembl_ids, index=adata.obs_names)
-
-    # Annotate with metadata
-    expr['cluster'] = adata.obs[cluster_key].values
-    expr['donor_id'] = adata.obs['donor_id'].values
-
-    # Melt for long format
-    df = expr.melt(id_vars=["cluster", "donor_id"],
-                   value_vars=ensembl_ids,
-                   var_name="gene_id", value_name="expression")\
-             .dropna(axis=0)
-
-    # Map gene id to feature name
-    df["gene_name"] = df["gene_id"].map(feature_map)
-    group_df = df.groupby(["cluster", "donor_id", "gene_name"])["expression"]\
-                 .mean().reset_index().dropna(subset=["expression"])
-    return group_df
-
-
-def run_kruskal_test(df):
-    grouped = [grp["expression"].values for _, grp in df.groupby("cluster")]
-    stat, p = kruskal(*grouped)
-    return stat, p
-
-
-def run_posthoc_dunn(df):
-    return sp.posthoc_dunn(df, val_col='expression', group_col='cluster',
-                           p_adjust='fdr_bh')
-
+#### Main
+input_file <- "../_m/old-version/pericyte.hlca_core.subclustered.h5ad"
+sce  <- zellkonverter::readH5AD(input_file)
+seur <- as.Seurat(sce, counts = "counts", data = "logcounts")
+seur$cell_type   <- forcats::fct_rev(factor(seur$cell_type))
+seur$compartment <- forcats::fct_rev(factor(seur$compartment))
+seur$subclusters <- forcats::fct_rev(factor(seur$subclusters))
+seur$clusters    <- forcats::fct_rev(factor(seur$clusters))
+seur$active.ident <- df.seurat$cell_type
+Idents(seur) <- "cell_type"
 
 def plot_boxplot_with_jitter(df, gene, stat, p_val, outdir):
     plt.figure(figsize=(7, 6))
