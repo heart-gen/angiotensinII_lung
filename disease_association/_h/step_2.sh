10f61b1ef8487f0b4fcc037fb71e35b7fc54d3a2
Kynon JM Benjamin <KrotosBenjamin@users.noreply.github.com>
2025-10-07 09:51:44 -0500

    Update step_2.sh
    Updating for bridges-2

--- disease_association/_h/step_2.sh
+++ disease_association/_h/step_2.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
-#SBATCH --partition=bluejay,shared
+#SBATCH --partition=RM-shared
 #SBATCH --job-name=ipf_dataset
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=jbenja13@jh.edu
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=50gb
-#SBATCH --output=conversion.log
+#SBATCH --mail-user=kj.benjamin90@gmail.com
+#SBATCH --ntasks-per-node=32
 #SBATCH --time=04:00:00
+#SBATCH --output=conversion.log
 
-echo "**** Job starts ****"
-date
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
 
-echo "**** JHPCE info ****"
+log_message "**** Job starts ****"
+
+log_message "**** Bridges-2 info ****"
 echo "User: ${USER}"
 echo "Job id: ${SLURM_JOBID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURM_NODENAME}"
 echo "Hostname: ${HOSTNAME}"
-echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 
 ## List current modules for reproducibility
 
-module load R
+module purge
+module load anaconda3/2024.10-1
 module list
 
 ## Edit with your job command
+log_message "**** Loading mamba environment ****"
+conda activate /ocean/projects/bio250020p/shared/opt/env/R_env
 
-echo "**** Run IPF conversion ****"
+log_message "**** Run IPF conversion ****"
 
 Rscript ../_h/02.generate_ipf_data.R
 
-echo "**** Job ends ****"
-date
+if [ $? -ne 0 ]; then
+    log_message "Error: Python execution failed"
+    exit 1
+fi
+
+conda deactivate
+log_message "**** Job ends ****"
