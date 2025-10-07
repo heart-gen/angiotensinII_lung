998f9b27fcbdb1a5df78616859446188f8ca5404
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-10-07 10:56:38 -0400

    adding download scrip for the IPF?COPD data

---
+++ inputs/ipf/_h/submit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE136nnn/GSE136831/suppl/GSE136831_AllCells.GeneIDs.txt.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE136nnn/GSE136831/suppl/GSE136831_AllCells.Samples.CellType.MetadataTable.txt.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE136nnn/GSE136831/suppl/GSE136831_AllCells.cellBarcodes.txt.gz
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE136nnn/GSE136831/suppl/GSE136831_RawCounts_Sparse.mtx.gz
+
